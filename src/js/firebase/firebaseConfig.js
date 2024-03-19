8e798da393898be4967580b88e0e1458179f9e2e
Victoriia D <eva_vica@i.ua>
2024-03-19 10:08:32 +0200

    update firebaseConfig

--- src/js/firebase/firebaseConfig.js
+++ src/js/firebase/firebaseConfig.js
@@ -10,3 +10,14 @@ export const firebaseConfig = {
   messagingSenderId: '501362930586',
   appId: '1:501362930586:web:387ce46ec4671a8d3f4023',
 };
+
+// export const firebaseConfig = {
+//   apiKey: 'AIzaSyCHTmc3Gh4W1XLIpMpgCM752wA_87tnpyE',
+//   authDomain: 'learn-lingo.firebaseapp.com',
+//   datafaseURL:
+//     '"https://learn-lingo-e82db-default-rtdb.europe-west1.firebasedatabase.app',
+//   projectId: 'learn-lingo-e82db',
+//   storageBucket: 'learn-lingo.appspot.com',
+//   messagingSenderId: '397520927845',
+//   appId: '1:501362930586:web:387ce46ec4671a8d3f4023',
+// };
